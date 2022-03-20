01076b876af2247efe98ab8087b8542e1e68cf61
Diana <dianapruteanu@ymail.com>
2022-03-20 17:27:47 +0200

    Am declarat și inițializat un set de variabile care vor defini proprietățile clasei persoană.

---
+++ src/topic2/Main.java
@@ -0,0 +1,11 @@
+package topic2;
+
+public class Main {
+    public static void main(String[] args) {
+        Persona marinelPersoana = new Persona();
+
+        marinelPersoana.numePersoana = "Marin";
+        marinelPersoana.seteazaVarsta(25);
+        System.out.println(marinelPersoana.getVarsta());
+    }
+}
